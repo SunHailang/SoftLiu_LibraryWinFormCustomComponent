a54611ca98078cec2f775039ba8f5a6384860005
HaiLangSun <hai-lang.sun@ubisoft.com>
2020-10-27 17:08:46 +0800

    update

---
+++ SoftLiu_LibraryWinFormCustomComponent/Button/CustomRadioButton.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Drawing;
+using System.ComponentModel;
+using System.Windows.Forms;
+using System.Drawing.Design;
+using System.Runtime.InteropServices;
+
+namespace SoftLiu_LibraryWinFormCustomComponent.Button
+{
+    [DefaultEvent("ReadOnlyChanged")]
+    public class CustomRadioButton : RadioButton
+    {
+        [DefaultValue(false)]
+        [RefreshProperties(RefreshProperties.Repaint)]
+        //[SRCategoryAttribute("CatBehavior")]
+        //[SRDescriptionAttribute("RadioButtonReadOnlyDescr")]
+        public bool ReadOnly { get; set; }
+
+        protected override void OnClick(EventArgs e)
+        {
+            if (!ReadOnly)
+                base.OnClick(e);
+        }
+
+        // 定义委托
+        public event EventHandler ReadOnlyChanged;
+        protected virtual void OnReadOnlyChanged(object sender, EventArgs e)
+        {
+            if (ReadOnlyChanged != null)
+            {
+                ReadOnlyChanged(sender, e);
+            }
+        }
+    }
+}
